23768c353e5f21bb88d072708832906dc0ffb394
Bridget Amana <amanabridget03@gmail.com>
2024-05-14 12:04:36 +0100

    completed the react router tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Index from "./routes/index";
+// Create and render a browser router in main.jsx
+import { createBrowserRouter, RouterProvider, Routes } from "react-router-dom";
+import Root, { loader as rootloader, action as rootAction } from "./routes/root";
+import ErrorPage from "./errror-page";
+import Contact, {loader as contactLoader} from "./routes/contact";
+import EditContact, {
+  action as editAction
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+// Create and render a browser router in main.jsx
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // Set <Root> as the root route's element
+    element: <Root />,
+    // Set the <ErrorPage> as the errorElement on the root route
+    errorElement: <ErrorPage />,
+    loader: rootloader,
+    action: rootAction,
+    // Nested Routes
+    children: [
+      {
+        index: true, element: <Index />,
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+  // create a new route
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* Create and render a browser router in main.jsx */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
